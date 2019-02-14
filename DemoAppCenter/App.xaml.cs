0b8e5d6b4a3520f2f86e3b20fed3aa33f39e57c4
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2019-02-14 08:49:29 -0200

    :tada: Primeira versão

---
+++ DemoAppCenter/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Distribute;
+using Microsoft.AppCenter.Push;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace DemoAppCenter
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            AppCenter.Start("android=35a65bbb-c439-4e0e-b34f-f98a4bb81376;" +
+                  "uwp={Your UWP App secret here};" +
+                  "ios={Your iOS App secret here}",
+                  typeof(Analytics), typeof(Crashes),typeof(Distribute),typeof(Push));
+           
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ DemoAppCenter/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Xamarin.Forms;
+
+namespace DemoAppCenter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Evento_Clicked(object sender, EventArgs e)
+        {
+        
+           var evento = new Dictionary<string, string>
+                    {
+                        { "Usuario", "Bertuzzi" },
+                        { "ClienteID", "1" }
+                    };
+
+            Analytics.TrackEvent("Login Efetuado", evento);
+        }
+
+        public void Erro_Clicked(object sender, EventArgs e)
+        {
+
+            Crashes.GenerateTestCrash();
+
+            try
+            {
+                List<string> teste = null;
+                teste.Add("Vai dar erro");
+            }
+            catch(Exception ex)
+            {
+                var properties = new Dictionary<string, string> {
+                    { "Category", "Tela de Login" },
+                    { "List", "Add" }
+                  };
+                Crashes.TrackError(ex, properties);
+            }
+        }
+    }
+}
